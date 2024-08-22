0cb9548bcb10813abd194d462cf1c0ba5e4166ee
hellowcoder <174770521+hellowcoder@users.noreply.github.com>
2024-08-22 15:03:19 +0530

    Update

---
+++ Arithmetic.h
@@ -0,0 +1,16 @@
+#ifndef ARITHMETIC_H
+#define ARITHMETIC_H
+
+// Function to add two numbers
+float add(float a, float b);
+
+// Function to subtract two numbers
+float subtract(float a, float b);
+
+// Function to multiply two numbers
+float multiply(float a, float b);
+
+// Function to divide two numbers
+float divide(float a, float b);
+
+#endif
