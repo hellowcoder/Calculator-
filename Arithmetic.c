c55f9a657aad225c0625ed2be76125f00fd39a5f
hellowcoder <174770521+hellowcoder@users.noreply.github.com>
2024-08-22 15:01:27 +0530

    Arithmetic.c

---
+++ Arithmetic.c
@@ -0,0 +1,24 @@
+#include "Arithmetic.h"
+
+// Function to add two numbers
+float add(float a, float b) {
+    return a + b;
+}
+
+// Function to subtract two numbers
+float subtract(float a, float b) {
+    return a - b;
+}
+
+// Function to multiply two numbers
+float multiply(float a, float b) {
+    return a * b;
+}
+
+// Function to divide two numbers
+float divide(float a, float b) {
+    if (b != 0)
+        return a / b;
+    else
+        return 0;  // Handle division by zero
+}
